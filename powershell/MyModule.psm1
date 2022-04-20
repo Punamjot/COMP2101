0a703e1325211fbee55f7c6a2302fcc8c9335556
Punamjot <Punamjot.Kaur@MyGeorgian.ca>
2022-04-20 13:46:14 -0400

    Create MyModule.psm1

---
+++ powershell/MyModule.psm1
@@ -0,0 +1,104 @@
+function welcome {
+write-output "Welcome to poershell $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "it is $now"
+}
+
+
+function get-cpuinfo {
+get-ciminstance cim_processor | format-list Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCores
+}
+
+
+function get-mydisks {
+get-disk | select-object Manufacturer, Model, SerialNumber, FirmwareRevision, Size
+}
+
+
+
+echo " ***__System_Hardware_Description__***"
+function HardwareDescription {
+Get-WmiObject -class win32_computersystem
+}
+
+
+
+
+echo " ***__Operating_System_Name_and_Version_Number__***"
+function OperatingSystem {
+Get-WmiObject -class win32_OperatingSystem | Format-list name, version
+}
+
+
+
+
+echo " ***__Processor_Description__***"
+function ProcessorDescription {
+Get-WmiObject -class win32_processor | Format-list  Description, MaxClockspeed, NumberOfCores, L3CacheSize, L3CacheSpeed
+}
+
+
+
+
+echo " ***__Summary_Of_RAM_installed__***"
+function RAMinstalled {
+Get-WmiObject -class win32_physicalmemory |
+foreach {
+         New-Object -TypeName psobject -Property @{
+             Manufacturer = $_.Manufacturer
+             Description = $_.Description
+             Banklabel = $_.BankLabel
+             Slot = $_.DeviceLocator
+             "Size(MB)"=$_.Capacity / 1mb -as [int]
+          }
+          $total = $_.Capacity / 1mb -as [int]
+ } |Format-Table Manufacturer, Description, BankLabel, Slot, "Size(MB)"
+ "totalRAM = $total MB"
+}
+
+
+
+
+echo " ***__Summary_Of_PhysicalDisk_drives__***"
+function PhysicalDisk {
+$diskdrives = Get-CIMInstance CIM_diskdrive
+
+  foreach ($disk in $diskdrives) {
+      $partitions = $disk|get-cimassociatedinstance -resultclassname CIM_diskpartition
+      foreach ($partition in $partitions) {
+            $logicaldisks = $partition | get-cimassociatedinstance -resultclassname CIM_logicaldisk
+            foreach ($logicaldisk in $logicaldisks) {
+                     new-object -typename psobject -property @{Manufacturer = $disk.Manufacturer
+                                                               Model = $disk.model
+                                                               Space = ($disk.size - $logicaldisk.FreeSpace) /1gb -as [int]
+                                                               FreeSpace = ($logicaldisk.FreeSpace) /1gb -as [int]
+                                                               "Size(GB)"=$disk.size / 1gb -as [int]
+                                                               } | Format-Table Manufacturer, Space, FreeSpace, "Size(GB)"
+           }
+      }
+  }
+}
+
+
+
+
+echo " ***__Network_Adapter_Configuration__***"
+function NetworkAdapter {
+get-ciminstance -class win32_networkadapterconfiguration | where-object ipenabled | format-table Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
+}
+
+
+
+
+echo " ***__Video_Card_Vendor_Description_and_CurrentScreenResolution__***"
+function VideoController {
+Get-WmiObject -class win32_Videocontroller |
+foreach {
+          New-Object -TypeName psobject -Property @{
+                     Description = $_.Description
+                     Manufacturer = $_.AdapterCompatibility
+                     VideoArchitecture = $_.VideoArchitecture
+                     CurrentScreenResolution = [string]$_.CurrentHorizontalResolution + "x" + $_.CurrentVerticalResolution
+  } | Format-List Description, Manufacturer, VideoArchitecture, CurrentScreenResolution
+  }
+  }
